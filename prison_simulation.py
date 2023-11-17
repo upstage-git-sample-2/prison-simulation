0db6b089a716fff0855206a04e6e17bd88d02772
Your Name Here <your_email@youremail.com>
2023-11-17 18:18:05 +0900

    Feat: Add select_prisoner function

--- prison_simulation.py
+++ prison_simulation.py
@@ -1,1 +1,8 @@
-print('hello')
+import random
+
+# 조성운
+def select_prisoner(prisoners, selected_prisoners):
+    # 죄수 선택 및 기록
+    selected_prisoner = random.choice(prisoners)
+    selected_prisoners.add(selected_prisoner)
+    return selected_prisoner
