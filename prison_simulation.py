c7048c1ddaf029d81f4e707dbd972e67b4f87fcd
Your Name Here <your_email@youremail.com>
2023-11-17 18:21:51 +0900

    Fix2: select_prisoner function

--- prison_simulation.py
+++ prison_simulation.py
@@ -1,12 +1,3 @@
-import random
-
-import random
-
-if __name__ == "__main__":
-    num_simulations = 1000
-    num_prisoners = 100
-    run_simulations(num_simulations, num_prisoners)
-    
 # 조성운
 def select_prisoner(prisoners, selected_prisoners):
     # 죄수 선택 및 기록
